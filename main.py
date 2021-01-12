40aaa522cdf80539e67630d002004f40cda90b0d
lostshadow <graph.rahal@live.fr>
2021-01-12 14:06:20 +0100

    commit initial ajout de fichier main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+def main():
+    casino = Casino(100000)
+    jeu1 = Roulette("my wheel", casino)
+    jeu2 = MachineASous("bandit manchot", casino)
+    joueur = Joueur("Clement", 1000)
+    joueur.entrer_casino(casino)
+    joueur.jouer()
+
