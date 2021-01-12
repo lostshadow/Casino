de754120a4518db8b69928d8957c3e2ebf35ca0d
lostshadow <graph.rahal@live.fr>
2021-01-12 15:04:52 +0100

    Ajout Class Casino. py Jeu.py Joueur.py MachineAsous.py Roulette.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+from Casino import Casino
+from Joueur import Joueur
+from MachineASous import MachineASous
+import Roulette
 
 
 def main():
     casino = Casino(100000)
-    jeu1 = Roulette("my wheel", casino)
+    jeu1 = Roulette.Roulette("my wheel", casino)
     jeu2 = MachineASous("bandit manchot", casino)
     joueur = Joueur("Clement", 1000)
     joueur.entrer_casino(casino)
     joueur.jouer()
+
+
+if __name__=='__main__':
+    main()
+
 
